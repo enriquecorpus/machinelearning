a164f46b88ffa424f11d7c80e7d6304ea38fc232
Enrique Corpus <enriquecorpus@skydev.solutions>
2020-03-12 21:18:26 +0800

    bald or not bald prediction

---
+++ identifier.py
@@ -0,0 +1,18 @@
+from imageai.Prediction.Custom import CustomImagePrediction
+import os
+import os.path
+
+execution_path = os.getcwd()
+
+prediction = CustomImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath('{path}/traineddata/model_ex-032_acc-0.765625.h5'.format(path=execution_path))
+prediction.setJsonPath('{path}/traineddata/model_class.json'.format(path=execution_path))
+prediction.loadModel(num_objects=2)  # number of trained objects
+
+
+def predict(img):
+    predictions, probabilities = prediction.predictImage(img, result_count=1)
+
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(str(eachPrediction) + " : " + str(eachProbability))
--- imagedownloader.py
+++ imagedownloader.py
@@ -5,5 +5,5 @@ keyword_ = input('Type keyword you want to download: ')
 google_crawler = GoogleImageCrawler(
     storage={'root_dir': '{f}/{keyword}/'.format(f=os.getcwd(), keyword=keyword_)}, parser_threads=2,
     downloader_threads=4)
-google_crawler.session.verify = False
+#google_crawler.session.verify = False
 google_crawler.crawl(keyword=keyword_, max_num=100, min_size=(100, 100), max_size=(200, 200))
