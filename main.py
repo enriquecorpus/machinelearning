fec789780b6656644061f1d2d7d12f22bb446fcd
Enrique Corpus <enriquecorpus@skydev.solutions>
2020-03-13 13:18:19 +0800

    check if image contains face

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import os
 import os.path
 from keras import backend as K
 import json
-
+from cv2 import imread
+from cv2 import CascadeClassifier
+# load the photograph
+# load the pre-trained model
 # ------------------------------------------------
 # gunicorn --workers=1 main:app (tensorflow backend)
 # -------------------------------------------------
@@ -19,6 +22,8 @@ prediction.setModelPath('{path}/traineddata/model_ex-032_acc-0.765625.h5'.format
 prediction.setJsonPath('{path}/traineddata/model_class.json'.format(path=execution_path))
 prediction.loadModel(num_objects=2)  # number of trained objects
 
+classifier = CascadeClassifier('haarcascade_frontalface_default.xml')
+
 
 def predict(img):
     # REMOVE THIS IN THE FUTURE, USE FILE STREAM
@@ -29,6 +34,10 @@ def predict(img):
     filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
     with open(filename, 'wb') as f:
         f.write(imgdata)
+
+    pixels = imread(filename)
+    if not classifier.detectMultiScale(pixels):
+        return {'Result': 'Unknown object'}
     predictions, probabilities = prediction.predictImage(image_input=filename, result_count=1)
 
     for eachPrediction, eachProbability in zip(predictions, probabilities):
